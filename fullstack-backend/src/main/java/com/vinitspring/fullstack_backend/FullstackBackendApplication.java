fc27d76072cf95d426a801c9000bbb98f17a0f34
Vinit <vinitsk99@gmail.com>
2024-07-21 11:53:11 +0530

    Add setup and configuration files

---
+++ fullstack-backend/src/main/java/com/vinitspring/fullstack_backend/FullstackBackendApplication.java
@@ -0,0 +1,14 @@
+package com.vinitspring.fullstack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackendApplication.class, args);	//start the sprig boot application
+
+	}
+
+}
