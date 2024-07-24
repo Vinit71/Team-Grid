19064d858c188b97afe75cdb3ffa2357d348178f
Vinit <vinitsk99@gmail.com>
2024-07-24 18:58:40 +0530

    Add endpoint to retrieve employee details by ID

--- fullstack-backend/src/main/java/com/vinitspring/fullstack_backend/controller/EmployeesController.java
+++ fullstack-backend/src/main/java/com/vinitspring/fullstack_backend/controller/EmployeesController.java
@@ -1,12 +1,10 @@
 package com.vinitspring.fullstack_backend.controller;
 
+import com.vinitspring.fullstack_backend.exception.EmployeeNotFoundException;
 import com.vinitspring.fullstack_backend.model.Employee;
 import com.vinitspring.fullstack_backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,13 @@ public class EmployeesController {
     @GetMapping("/getemployees")    //retrieve details of all employees
     List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    //Update employee details
+    @GetMapping("/employees/updaterecords/{id}")
+    Employee getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id)
+        .orElseThrow(() -> new EmployeeNotFoundException(id));
     }
 }
 
