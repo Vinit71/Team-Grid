8833d0281a83795810da88318d1519570ce0a6fb
Vinit <vinitsk99@gmail.com>
2024-07-31 19:24:21 +0530

    Add endpoint to delete employee by ID with existence check

--- fullstack-backend/src/main/java/com/vinitspring/fullstack_backend/controller/EmployeesController.java
+++ fullstack-backend/src/main/java/com/vinitspring/fullstack_backend/controller/EmployeesController.java
@@ -14,6 +14,7 @@ public class EmployeesController {
     @Autowired  //automatic dependency injection
     private EmployeeRepository employeeRepository;
 
+
     @PostMapping ("/employee")   //add a new employee
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return employeeRepository.save(newEmployee);
@@ -24,8 +25,30 @@ public class EmployeesController {
         return employeeRepository.findAll();
     }
 
-    //Update employee details
-    @GetMapping("/employees/updaterecords/{id}")
+    //Update Employee details by ID
+    @PutMapping("/employees/updaterecords/{id}")
+    Employee updateEmployeeDetails(@RequestBody Employee currEmp,@PathVariable Long id){
+        return employeeRepository.findById(id)
+                .map(employee -> {
+//                    employee.setFullName(currEmp.getFullName());
+                    employee.setEmail(currEmp.getEmail());
+                    employee.setPosition(currEmp.getPosition());
+                    return employeeRepository.save(employee);
+                }).orElseThrow(()-> new EmployeeNotFoundException(id));
+    }
+
+    //Delete an employee records
+    @DeleteMapping("/employee/delete/{id}")
+    String deleteEmployee(@PathVariable Long id){
+        if(!employeeRepository.existsById(id)){
+            throw new EmployeeNotFoundException(id);
+        }
+        employeeRepository.deleteById(id);
+        return "Employee records deleted Successfully";
+    }
+
+    //Retrieve employee details by ID
+    @GetMapping("/employees/single/{id}")
     Employee getEmployeeById(@PathVariable Long id) {
         return employeeRepository.findById(id)
         .orElseThrow(() -> new EmployeeNotFoundException(id));
